2eae82d9e41046533b432ad540efabe5433c0058
SanduniAmanda <amandasanduni01@gmail.com>
2026-06-13 14:48:06 +0530

    Add files via upload

---
+++ ICA 2/script.js
@@ -0,0 +1,21 @@
+function submitBooking()
+{
+	let name= document.getElementById("name").value;
+	let email=document.getElementById("email").value;
+	let phone=document.getElementById("phone").value;
+	let room=document.getElementById("room").value;
+	
+	let payment=document.querySelector('input[name="payment"]:checked');
+	
+	let paymentMethod= document.querySelector('input[name="payment"]:checked');
+					//condition ? value_if_true: value_if_false;
+	
+	document.getElementById("result").innerHTML=
+		"<p>Name: " + name + "</p>" +
+		"<p>Email: " + email + "</p>" +
+		"<p>Phone: " + phone + "</p>" +
+		"<p>Room Type: " + room +"</p>"
+		"<p>Payment Method: " + paymentMethod + "</p>";
+		
+	alert("Ordered Successfully!");
+}
